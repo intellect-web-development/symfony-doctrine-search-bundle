1843681e00147e49faa80bd0bed928b25b922abf
alexander <sanyaerin@mail.ru>
2024-10-29 10:56:12 +0300

    wip

--- src/DependencyInjection/SymfonyDoctrineSearchExtension.php
+++ src/DependencyInjection/SymfonyDoctrineSearchExtension.php
@@ -7,7 +7,7 @@ namespace IWD\SymfonyDoctrineSearch\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class SymfonyDoctrineSearchExtension extends Extension
 {
